4ac36f5e84486e797878c2ed734246d56cd54e54
unknown <daveasmah@yahoo.com>
2023-11-01 08:21:31 -0400

    Complete qualified assignment, ...

---
+++ public/src/accounts.js
@@ -0,0 +1,71 @@
+function findAccountById(accounts, id) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+  const foundAccount = accounts.find((account) => account.id === id)
+  return foundAccount;
+}
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  
+  function compareByLastName(a, b) {
+    const lastNameA = a.name.last.toLowerCase();
+    const lastNameB = b.name.last.toLowerCase();
+
+    if (lastNameA < lastNameB) {
+      return -1;
+    }
+    if (lastNameA > lastNameB) {
+      return 1;
+    }
+    return 0;
+  }
+
+  // Sort the array by last name
+  accounts.sort(compareByLastName);
+
+  return accounts; // Return the sorted array
+}
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  const getFullNames = accounts.map((account) => {
+    const firstName = account.name.first;
+    const lastName = account.name.last;
+    return `${firstName} ${lastName}`;
+  });
+  return getFullNames;
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
